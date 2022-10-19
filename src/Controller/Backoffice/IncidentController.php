ab27109096027b05c02c4c5abb24c6b12c61fad6
JPechberty <j.pechberty@gmail.com>
2022-10-19 14:45:55 +0200

    Secure Incident controller logic

--- src/Controller/Backoffice/IncidentController.php
+++ src/Controller/Backoffice/IncidentController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\throwException;
 
 #[Route('/backoffice/incidents')]
 class IncidentController extends AbstractController
@@ -25,6 +26,7 @@ class IncidentController extends AbstractController
     #[Route('/{id}', name: 'app_backoffice_incident_show', methods: ['GET'])]
     public function show(Incident $incident): Response
     {
+
         return $this->render('backoffice/incident/show.html.twig', [
             'incident' => $incident,
         ]);
@@ -33,24 +35,40 @@ class IncidentController extends AbstractController
     #[Route('/{id}/process', name: 'app_backoffice_incident_process', methods: ['GET'])]
     public function markAsProcessed(Incident $incident, IncidentRepository $incidentRepository): Response
     {
-        $incident->setProcessedAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
 
-        $incidentRepository->save($incident,true);
+        if(!$incident->getProcessedAt()){
+            $incident->setProcessedAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
+            $incidentRepository->save($incident,true);
+        }else{
+            $this->addFlash(
+                'error',
+                "Incident ".$incident->getReference(). "already processed."
+            );
+        }
 
-        return $this->render('backoffice/incident/show.html.twig', [
-            'incident' => $incident,
+        return $this->redirectToRoute('app_backoffice_incident_show', [
+            'id' => $incident->getId()
         ]);
     }
 
     #[Route('/{id}/resolve', name: 'app_backoffice_incident_resolve', methods: ['GET'])]
     public function markAsResolved(Incident $incident, IncidentRepository $incidentRepository): Response
     {
-        $incident->setResolveAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
 
-        $incidentRepository->save($incident,true);
+        if($incident->getProcessedAt() && !$incident->getResolveAt() && !$incident->getRejectedAt()){
+            $incident->setResolveAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
+            $incidentRepository->save($incident,true);
+        }else{
+            $this->addFlash(
+                'error',
+                "Incident ".$incident->getReference(). "is not processed or is already resolved or is already rejected."
+            );
+        }
 
-        return $this->render('backoffice/incident/show.html.twig', [
-            'incident' => $incident,
+
+
+        return $this->redirectToRoute('app_backoffice_incident_show', [
+            'id' => $incident->getId()
         ]);
     }
 
@@ -58,12 +76,20 @@ class IncidentController extends AbstractController
     #[Route('/{id}/reject', name: 'app_backoffice_incident_reject', methods: ['GET'])]
     public function markAsRejected(Incident $incident, IncidentRepository $incidentRepository): Response
     {
-        $incident->setRejectedAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
+        if(!$incident->getResolveAt() && !$incident->getRejectedAt()){
+            $incident->setRejectedAt(new DateTimeImmutable('now',new \DateTimeZone('Europe/Paris')));
+            $incidentRepository->save($incident,true);
+        }else{
+            $this->addFlash(
+                'error',
+                "Incident ".$incident->getReference(). " is already resolved or is already rejected."
+            );
+        }
 
-        $incidentRepository->save($incident,true);
 
-        return $this->render('backoffice/incident/show.html.twig', [
-            'incident' => $incident,
+
+        return $this->redirectToRoute('app_backoffice_incident_show', [
+            'id' => $incident->getId()
         ]);
     }
 
